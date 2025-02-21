a050f448c1d1940d9d379a164d077274776d16c4
parsa <parsabarqi@gmail.com>
2025-02-22 00:55:35 +0330

    implement echo

--- app/main.py
+++ app/main.py
@@ -1,36 +1,64 @@
 import sys
+from typing import NoReturn
 
-def user_command() -> str:
-    """Use $ for the first writing letter"""
-    try:
-        sys.stdout.write("$ ")
-        sys.stdout.flush()
-        return sys.stdin.readline().strip()
+
+def handle_inputline(inputline: str) -> None:
+    """parse and handle the codes and text from input line"""
     
-    except KeyboardInterrupt:
-        print(f"Exiting from the shell")
+    code, *args = inputline.strip().split(maxsplit=1) # make command to two part
+    args = args[0].split() if args else []
+    
+    match code:
+        case "exit":
+            handle_exit()
+        case "echo":
+            handle_echo(args)
+        case _: 
+            sys.stdout.write(f"{inputline}: command not found\n")
 
-def handle_command(command: str) -> str:
-    """Handle user inputs and commands"""
-    try: 
-        if not command:
-            return 
-        sys.stdout.write(f"{command}: command not found \n")
-        sys.stdout.flush()
+            
+def handle_echo(args: list[str]) -> None:
+    
+    sys.stdout.write(f"{''.join(args)}\n")
         
-    except Exception as e:
-        print(f"exception error: {e}")
+            
+def handle_exit(code: str = "0") -> NoReturn:
+    """Handle exit command and the optional status code"""
+    try:
+        sys.exit()
+    except ValueError:
+        sys.exit(f"invalid exit code: {code}")
+    
+
+# def handle_command(command: str) -> str:
+#     """Handle user inputs and commands"""
+#     default_commands = ['exit', 'quit', 'type', 'echo']
+#     try: 
+#         if not command:
+#             return
+#         elif "echo" in command:
+#             sys.stdout.write(f"{command.replace('echo','').lstrip()}\n")
+#             sys.stdout.flush()
+#         else:
+#             sys.stdout.write(f"{command}: command not found \n")
+#             sys.stdout.flush()
+        
+#     except Exception as e:
+#         print(f"exception error: {e}")
+
 
 
 def main():
+    """REPL Loop"""
     # Uncomment this block to pass the first stage
     # sys.stdout.write("$ ")
-    while True: 
-        command = user_command()
-        if command.lower() in ('exit 0', 'quit'):
-            break
-        handle_command(command)
-    
+    while True:
+        try:
+            handle_inputline(input("$ "))
+        except (EOFError, KeyboardInterrupt):
+            sys.stderr("\n Exiting")
+        except Exception as e:
+            sys.stderr(f"exception error: {e}")
     # Wait for user input
     # new_command = input()
     
