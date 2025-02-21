9fcbba525e5ec6ac1832a55cb0b31e1dbb9797ba
parsa <parsabarqi@gmail.com>
2025-02-21 21:26:30 +0330

    third

--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
 import sys
 
+def isInvalid() -> str:
+    words = sys.stdin
+    return words
 
 def main():
     # Uncomment this block to pass the first stage
     sys.stdout.write("$ ")
-
+    
     # Wait for user input
-    input()
-
+    new_command = input()
+    print(f"{new_command}: command not found")
 
 if __name__ == "__main__":
     main()
